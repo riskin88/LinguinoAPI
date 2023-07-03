8e6bfded3b3217f402ed8fed919db830d0f4c07b
Jan Hlaváč <hlavac.honza@email.cz>
2023-07-03 19:05:12 +0200

    create extension method for DI

---
+++ LinguinoAPI/DependencyInjection/DI.cs
@@ -0,0 +1,18 @@
+﻿using BLL.Services;
+using BLL.Services.Contracts;
+using DAL.UnitOfWork;
+
+namespace LinguinoAPI.DependencyInjection
+{
+    public static class DI
+    {
+        public static IServiceCollection AddCustomServices(this IServiceCollection services)
+        {
+            services.AddScoped<IUserAuthService, UserAuthService>();
+            services.AddScoped<IUnitOfWork, UnitOfWork>();
+
+
+            return services;
+        }
+    }
+}
--- LinguinoAPI/Program.cs
+++ LinguinoAPI/Program.cs
@@ -5,6 +5,7 @@ using Microsoft.EntityFrameworkCore;
 using BLL.Services.Contracts;
 using BLL.Services;
 using DAL.UnitOfWork;
+using LinguinoAPI.DependencyInjection;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -12,8 +13,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddDbContext<DataContext>(o => o.UseSqlServer(builder.Configuration.GetConnectionString("MSSQL_DB")));
-builder.Services.AddScoped(typeof(IUnitOfWork), typeof(UnitOfWork));
-builder.Services.AddScoped(typeof(IUserService), typeof(UserService));
+builder.Services.AddCustomServices();
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
